3058d77fa1864a957eb3369f893b305182d2c3fd
TirsoCode <tirso@tirso.com>
2026-09-07 11:51:48 +0000

    feat: startupbar widget en head del sitio

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
+        <script async src="https://startupbar.co/widget/loader.js" data-startup-id="c3bdaa88-69ea-4a3d-8d75-fe3b09f420f0"></script>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'><rect width='32' height='32' rx='6' fill='%231A1918'/><text x='16' y='22' text-anchor='middle' font-size='18' fill='white' font-family='serif'>R</text></svg>" />
       </head>
       <body
